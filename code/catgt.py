97b62232709b76232af3d8fe6ce170f6d9775aee
Ben Heasly <benjamin.heasly@gmail.com>
2025-11-19 14:55:07 -0500

    catgt wrapper search input_root for a single subdir as the SpikeGlx run dir, when --run is omitted.

--- code/catgt.py
+++ code/catgt.py
@@ -4,13 +4,9 @@ from argparse import ArgumentParser
 import re
 from typing import Optional, Sequence
 import logging
-from datetime import datetime
-from zoneinfo import ZoneInfo
 from pathlib import Path
 import subprocess
-import json
 
-from files import find_one
 
 catgt_version = environ.get("CATGT_VERSION", "unknown/local")
 
@@ -78,9 +74,10 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
         help="directory to contain CatGT outputs for the run"
     )
     parser.add_argument(
-        "run",
+        "--run",
         type=str,
-        help="SpikeGLX run name, for example 'test'"
+        help="SpikeGLX run name, for example 'AS20_03112025_trainingSingle6Tone2024_Snk3.1'.  Can omit this if INPUT_ROOT contains only one subdir, like 'AS20_03112025_trainingSingle6Tone2024_Snk3.1_g0'",
+        default=None
     )
     parser.add_argument(
         "--gate",
@@ -113,12 +110,29 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     output_path = Path(cli_args.output_root)
     output_path.mkdir(exist_ok=True, parents=True)
 
+    if cli_args.run:
+        run_name = cli_args.run
+    else:
+        logging.info(f"Searching for SpikeGlx run dir within input dir {input_path}")
+        subdir_names = [subdir.name for subdir in input_path.iterdir() if subdir.is_dir()]
+        logging.info(f"Found {len(subdir_names)} subdirs: {subdir_names}")
+        if not subdir_names:
+            raise ValueError("Input dir has no SpikeGlx run subdirectory.")
+        elif len(subdir_names > 1):
+            raise ValueError("Input dir has multiple subdirectories, but no SpikeGlx run was specified.")
+        else:
+            subdir_name = subdir_names[0]
+            gate_suffix = f"_g{cli_args.gate}"
+            logging.info(f"Using subdir name {subdir_name} as the SpikeGlx run name (minus gate suffix {gate_suffix})")
+            run_name = subdir_name.removesuffix(gate_suffix)
+    logging.info(f"Using SpikeGlx run name {run_name}")
+
     probe_index = re.search(r"\d+$", cli_args.probe_id).group()
     try:
         run_catgt(
             input_path,
             output_path,
-            cli_args.run,
+            run_name,
             cli_args.gate,
             cli_args.trigger,
             probe_index,
