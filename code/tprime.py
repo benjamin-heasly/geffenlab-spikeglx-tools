77d5f02b811e0787ab9f0399e0275f2eb49bd391
Ben Heasly <benjamin.heasly@gmail.com>
2026-04-14 12:42:45 -0400

    Modify tprime wrapper to handle multiple probes and parallel run dirs from CatGT and Kilosort/Phy.

--- code/tprime.py
+++ code/tprime.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 from typing import Optional, Sequence, Any
 import logging
 from pathlib import Path
-from shutil import copy2, copytree
+from shutil import copy2
 import subprocess
 
 import numpy as np
@@ -77,180 +77,263 @@ def phy_spike_times_to_samples(
 
 
 def run_tprime(
-    catgt_root: str,
-    phy_root: str,
-    output_root: str,
-    to_stream: str,
-    from_streams: list[tuple[str, str]],
-    phy_from_pattern: str,
-    phy_pattern: str,
+    catgt_run_path: Path,
+    phy_run_path: Path,
+    output_run_path: Path,
+    to_sync_path: Path,
+    events_sync_path: Path,
+    events_paths: list[Path],
+    probe_sync_paths: list[Path],
+    probe_spikes_paths: list[Path],
+    probe_params_paths: list[Path],
     sync_period: float,
-    offsets_file: str,
-    runit_path: str
-) -> int:
-    """Use TPrime to find and adjust event and spike times, and write adjusted times to the output root."""
+    offsets: str,
+    runit: str
+):
+    """Use TPrime to adjust event and spike times, write adjusted events to the output root and adjusted spikes in place."""
+    output_run_path.mkdir(parents=True, exist_ok=True)
 
     # Start building up TPrime command args.
-    catgt_path = Path(catgt_root).absolute()
-    to_stream_path = find_one(to_stream, parent=catgt_path)
     tprime_command = [
-        runit_path,
+        runit,
         f"-syncperiod={sync_period}",
-        f"-tostream={to_stream_path.as_posix()}",
+        f"-tostream={to_sync_path.as_posix()}",
     ]
 
     # For multi-part recordings, TPrime can read an "offsets" file produced by CatGT.
-    if offsets_file is not None:
-        offset_arg = f"-offsets={offsets_file}"
+    if offsets is not None:
+        offset_arg = f"-offsets={offsets}"
         tprime_command.append(offset_arg)
 
-    output_path = Path(output_root).absolute()
-    output_path.mkdir(parents=True, exist_ok=True)
+    # TPrime keeps track of clocks/streams by index.
+    # We'll use one for auxiliary events, plus one per probe.
+    stream_index = 0
 
-    # Locate CatGT results with sync events for each probe.
-    phy_path = Path(phy_root).absolute()
-    probe_mapping = {}
-    phy_from_matches = find(phy_from_pattern, parent=catgt_path)
-    phy_base_index = len(from_streams)
-    for probe_index, phy_from_path in enumerate(phy_from_matches):
-        logging.info(f"Converting spike times based on probe sync events: {phy_from_path}")
+    # Tell TPrime about auxiliary event sync pulses.
+    aux_from_arg = f"-fromstream={stream_index},{events_sync_path.as_posix()}"
+    tprime_command.append(aux_from_arg)
 
-        # Take the probe name from the end of the sync event parent dir name.
-        probe_parent_parts = phy_from_path.parent.name.split("_")
-        probe_name = probe_parent_parts[-1]
-        logging.info(f"Using probe name: {probe_name}")
+    # Tell TPrime which auxiliary events to convert, and where.
+    for events_path in events_paths:
+        events_relative_path = events_path.relative_to(catgt_run_path)
+        events_output_path = Path(output_run_path, events_relative_path)
+        events_output_path.parent.mkdir(parents=True, exist_ok=True)
+        aux_events_arg = f"-events={stream_index},{events_path.as_posix()},{events_output_path.as_posix()}"
+        tprime_command.append(aux_events_arg)
 
-        # Locate a Phy subdir for this probe and choose an output path with the same structure.
-        params_py = find_one(phy_pattern, probe_name, parent=phy_path)
-        params_py_relative = params_py.relative_to(phy_path)
-        phy_output_path = Path(output_path, params_py_relative.parent)
-        logging.info(f"Writing converted spike times for probe {probe_name} to {phy_output_path}")
-        phy_output_path.mkdir(exist_ok=True, parents=True)
+    # Organize Phy files per probe.
+    probe_seconds_to_convert = []
+    for probe_sync_path, probe_spikes_path, probe_params_path in zip(probe_sync_paths, probe_spikes_paths, probe_params_paths):
 
-        # Make a copy of the original spike_times.npy for reference.
-        spike_times_npy = Path(params_py.parent, "spike_times.npy")
-        spike_times_original = Path(phy_output_path, "spike_times_original.npy")
-        logging.info(f"Copying {probe_name} spike times {spike_times_npy} to {spike_times_original}.")
-        copy2(spike_times_npy, spike_times_original)
+        # Declare a new clock/stream for this probe.
+        stream_index += 1
 
-        # Convert Phy's spike times, in samples, to seconds for TPrime.
-        spike_times_seconds = Path(phy_output_path, "spike_times_sec_original.npy")
-        phy_spike_times_to_seconds(params_py, spike_times_original, spike_times_seconds)
+        probe_run_path = probe_params_path.parent
+        logging.info(f"Aligning spikes for probe: {probe_run_path}")
 
-        # Which clock / time stream / sync events file do spike times come from?
-        phy_index = phy_base_index + probe_index
-        from_stream_arg = f"-fromstream={phy_index},{phy_from_path.as_posix()}"
-        tprime_command.append(from_stream_arg)
+        probe_run_relative_path = probe_run_path.relative_to(phy_run_path)
+        probe_run_output_path = Path(output_run_path, probe_run_relative_path)
+        probe_run_output_path.mkdir(exist_ok=True, parents=True)
+        logging.info(f"Writing tprime results for probe: {probe_run_output_path}")
 
-        # Write adjusted spike times, in seconds, to the outputs dir.
-        spike_times_seconds_adjusted = Path(phy_output_path, "spike_times_sec_adj.npy")
-        event_arg = f"-events={phy_index},{spike_times_seconds.as_posix()},{spike_times_seconds_adjusted.as_posix()}"
-        tprime_command.append(event_arg)
+        # Make a backup copy of the original spike times, in place.
+        # Only do this once, so we don't clobber the backup with an adjusted version of spike_times.npy
+        # Going forward we will:
+        #   - read original spike times from the backup, spike_times_original.npy
+        #   - write adjusted spike times in place of the original, spike_times.npy
+        # This approach should make it safe to re-run this pipeline step.
+        # Modifying spike_times.npy in place is useful for downstream Phy and Bombcell.
+        # We'll write other outputs to a seprate folder, which is generally safer and cleaner.
+        probe_spikes_backup_path = probe_spikes_path.with_name("spike_times_original.npy")
+        if probe_spikes_backup_path.exists():
+            logging.info(f"Found existing spike times backup: {probe_spikes_backup_path}")
+        else:
+            logging.info(f"Creating spike times backup: {probe_spikes_backup_path}")
+            copy2(probe_spikes_path, probe_spikes_backup_path)
 
-        # After TPrime runs, convert the adjuste spike times in seconds, back to units of samples for Phy.
-        probe_mapping[params_py] = spike_times_seconds_adjusted
+        # Phy deals with spike times in sample numbers.  Convert to seconds for TPrime.
+        probe_spikes_seconds_path = Path(probe_run_output_path, "spike_times_sec_original.npy")
+        phy_spike_times_to_seconds(probe_params_path, probe_spikes_backup_path, probe_spikes_seconds_path)
 
-    for index, (from_glob, events_glob) in enumerate(from_streams):
-        # Which clock / time stream / sync events file do these events come from?
-        from_path = find_one(from_glob, parent=catgt_path)
-        from_stream_arg = f"-fromstream={index},{from_path.as_posix()}"
-        tprime_command.append(from_stream_arg)
+        # Tell TPrime about sync pulses for this probe.
+        probe_from_arg = f"-fromstream={stream_index},{probe_sync_path.as_posix()}"
+        tprime_command.append(probe_from_arg)
 
-        # Write adjusted events to the output_root.
-        events_paths = catgt_path.glob(events_glob)
-        for events_path in events_paths:
-            events_adjusted = Path(output_path, events_path.relative_to(catgt_path))
-            events_adjusted.parent.mkdir(parents=True, exist_ok=True)
-            event_arg = f"-events={index},{events_path.as_posix()},{events_adjusted.as_posix()}"
-            tprime_command.append(event_arg)
+        # Tell TPrime which spike times to convert, and where.
+        probe_spikes_seconds_adjusted_path = Path(probe_run_output_path, "spike_times_sec_adjusted.npy")
+        probe_events_arg = f"-events={stream_index},{probe_spikes_seconds_path.as_posix()},{probe_spikes_seconds_adjusted_path.as_posix()}"
+        tprime_command.append(probe_events_arg)
+
+        # After TPrime runs, convert the adjusted spike times in seconds back to samples.
+        probe_seconds_to_convert.append((probe_spikes_seconds_adjusted_path, probe_spikes_path, probe_params_path))
 
     logging.info(f"Running TPrime with command {tprime_command}")
-
-    result = subprocess.run(tprime_command, check=False, cwd=output_path)
-    tprime_log = Path(output_path, "TPrime.log")
+    result = subprocess.run(tprime_command, check=False, cwd=output_run_path)
+    tprime_log = Path(output_run_path, "TPrime.log")
     logging.info(f"Reading from TPrime log '{tprime_log}'")
     with open(tprime_log, 'r') as log:
         for line in log:
             print(line)
 
-    if result.returncode == 0:
-        for params_py, spike_times_seconds_adjusted in probe_mapping.items():
-            spike_times_adj_npy = Path(spike_times_seconds_adjusted.parent, "spike_times_adj.npy")
-            phy_spike_times_to_samples(params_py, spike_times_seconds_adjusted, spike_times_adj_npy)
+    if result.returncode != 0:
+        raise ValueError(f"Error running TPrime, please see log above.")
 
-            # Make a full copy of the input phy/ dir, then replace its spike_times.npy with the adjusted spike_times_adj.npy.
-            logging.info(f"Copying contents of phy/ dir from {params_py.parent} to {spike_times_seconds_adjusted.parent}")
-            copytree(params_py.parent, spike_times_seconds_adjusted.parent, dirs_exist_ok=True)
-
-            spike_times_npy = Path(params_py.parent, "spike_times.npy")
-            logging.info(f"Replacing original spike times in {spike_times_npy} with adjusted {spike_times_adj_npy}")
-            copy2(spike_times_adj_npy, spike_times_npy)
-
-    return result.returncode
+    for (probe_spikes_seconds_adjusted_path, probe_spikes_path, probe_params_path) in probe_seconds_to_convert:
+        logging.info(f"Updating original spike times in place: {probe_spikes_path}")
+        phy_spike_times_to_samples(probe_params_path, probe_spikes_seconds_adjusted_path, probe_spikes_path)
 
 
-def from_with_events(value: str):
-    parts = value.split(":")
-    return (parts[0], parts[1])
+def find_runs_and_align(
+    catgt_path: Path,
+    phy_path: Path,
+    output_path: Path,
+    to_sync_pattern: str,
+    events_sync_pattern: str,
+    events_pattern: str,
+    probe_ids: list[str],
+    probe_sync_pattern: str,
+    probe_spikes_pattern: str,
+    probe_params_pattern: str,
+    sync_period: float,
+    offsets: str,
+    runit: str,
+) -> int:
+    """Locate SpikeGlx/CatGT runs and corresponding sorting results, invoke TPrime for each run in sequence."""
+
+    # Locate SpikeGlx runs as subfolders of the CATGT_ROOT.
+    logging.info(f"Processing SpikeGlx/CatGT runs within: {catgt_path}")
+    catgt_run_paths = [run_dir for run_dir in catgt_path.iterdir() if run_dir.is_dir()]
+    logging.info(f"Found {len(catgt_run_paths)} CatGT run dirs: {catgt_run_paths}")
+    if not catgt_run_paths:
+        raise ValueError("Found no CatGT run dirs to process.")
+
+    for catgt_run_path in catgt_run_paths:
+        logging.info(f"Processing CatGT run dir: {catgt_run_path}")
+
+        to_sync_path = find_one(to_sync_pattern, parent=catgt_run_path)
+        logging.info(f"Aligning spikes and event TO desination sync pulses: {to_sync_path}")
+
+        events_sync_path = find_one(events_sync_pattern, parent=catgt_run_path)
+        logging.info(f"Aligning auxiliary events FROM sync pulses: {events_sync_path}")
+
+        events_paths = find(events_pattern, parent=catgt_run_path)
+        logging.info(f"Aligning auxiliary events FROM {len(events_paths)} original sources: {events_paths}")
+
+        probe_sync_paths = []
+        probe_spikes_paths = []
+        probe_params_paths = []
+        phy_run_path = Path(phy_path, catgt_run_path.name)
+        for probe_id in probe_ids:
+            logging.info(f"Looking for probe {probe_id} in run dir: {phy_run_path}")
+
+            probe_sync_path = find_one(probe_sync_pattern, filter=probe_id, parent=catgt_run_path, none_ok=True)
+            if probe_sync_path is not None:
+                logging.info(f"Aligning spikes FROM sync pulses: {probe_sync_path}")
+                probe_sync_paths.append(probe_sync_path)
+
+            probe_spikes_path = find_one(probe_spikes_pattern, filter=probe_id, parent=phy_run_path, none_ok=True)
+            if probe_spikes_path is not None:
+                logging.info(f"Aligning spikes FROM : {probe_spikes_path}")
+                probe_spikes_paths.append(probe_spikes_path)
+
+            probe_params_path = find_one(probe_params_pattern, filter=probe_id, parent=phy_run_path, none_ok=True)
+            if probe_params_path is not None:
+                logging.info(f"Found params.py : {probe_params_path}")
+                probe_params_paths.append(probe_params_path)
+
+        output_run_path = Path(output_path, catgt_run_path.name)
+        run_tprime(
+            catgt_run_path,
+            phy_run_path,
+            output_run_path,
+            to_sync_path,
+            events_sync_path,
+            events_paths,
+            probe_sync_paths,
+            probe_spikes_paths,
+            probe_params_paths,
+            sync_period,
+            offsets,
+            runit
+        )
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     set_up_logging()
 
-    parser = ArgumentParser(description="Use TPrime to align event and spike times to a common clock / time stream.")
+    parser = ArgumentParser(description="Use TPrime to align event and spike times to a destination clock.")
     parser.add_argument(
         "catgt_root",
         type=str,
-        help="directory to search for input event text files, as from CatGT"
+        help="directory with CatGT outputs from one or more runs"
     )
     parser.add_argument(
         "phy_root",
         type=str,
-        help="directory to search for input spike times files, in Phy format"
+        help="directory with Phy/Kilosort outputs from one or more runs"
     )
     parser.add_argument(
         "output_root",
         type=str,
-        help="directory to write adjusted event and spike times files."
+        help="directory to write adjusted event and spike times from each run."
     )
     parser.add_argument(
-        "--to-stream", "-t",
+        "--to-sync-pattern",
         type=str,
-        help="glob to match one text or .npy file with sync event pulses for the destination clock / time stream",
+        help="glob to match one text or .npy file of sync pulse times for the destination clock, within CATGT_ROOT, for each run. (default: %(default)s)",
+        default="*/*.imec0.ap.*.txt"
     )
     parser.add_argument(
-        "--sync-period", "-s",
+        "--events-sync-pattern",
+        type=str,
+        help="glob to match one text or .npy file of sync pulse times for auxiliary events (eg nidq or onebox), within CATGT_ROOT, for each run. (default: %(default)s)",
+        default="*.nidq.xd_8_4_500.txt"
+    )
+    parser.add_argument(
+        "--events-pattern",
+        type=str,
+        help="glob to match multiple text or .npy files of auxiliary event times (eg nidq or onebox), within CATGT_ROOT, for each run. (default: %(default)s)",
+        default="*.nidq.*.txt"
+    )
+    parser.add_argument(
+        "--probe-ids",
+        type=str,
+        nargs="+",
+        help="One or more probe ids to consider for sorting and for associating CatGT outputs with Kilosort outputs for the same probe. (default: %(default)s)",
+        default=["imec0", "imec1"]
+    )
+    parser.add_argument(
+        "--probe-sync-pattern",
+        type=str,
+        help="glob to match one text or .npy file of sync pulse times per probe, within CATGT_ROOT, for each run. (default: %(default)s)",
+        default="*/*.ap.*.txt"
+    )
+    parser.add_argument(
+        "--probe-spikes-pattern",
+        type=str,
+        help="glob to match one .npy file of spike event times per probe, within PHY_ROOT, for each run. (default: %(default)s)",
+        default="*/spike_times.npy"
+    )
+    parser.add_argument(
+        "--probe-params-pattern",
+        type=str,
+        help="glob to match one params.py file per probe, within PHY_ROOT, for each run. (default: %(default)s)",
+        default="*/params.py"
+    )
+    parser.add_argument(
+        "--sync-period",
         type=float,
-        help="nominal period in seconds of sync events (default 1.0s)",
+        help="nominal period in seconds of sync events. (default: %(default)s)",
         default=1.0
     )
     parser.add_argument(
-        "--from-streams", "-f",
-        type=from_with_events,
-        nargs="+",
-        help='one or more from:events pairs separated by spaces -- each "from" part is a glob to match one text or .npy file with sync event pulses for a source clock / time stream : each "events" part is a pattern for matching other event files, to be converted from the "from" clock to the TO_STREAM clock.  These patterns are matched within CATGT_ROOT.  For example: --from-steams ./nidq/sync.txt:/nidq/**/*.txt ./foo/foo.txt:/foo/*.npy',
-        default=[]
-    )
-    parser.add_argument(
-        "--phy-from-pattern", "-F",
-        type=str,
-        help="glob to match text file(s) with probe(s) sync event pulses, within CATGT_ROOT (default None)",
-        default="**/*.ap.*.txt"
-    )
-    parser.add_argument(
-        "--phy-pattern", "-y",
-        type=str,
-        help='Glob pattern for finding Phy params.py file(s) within PHY_ROOT. (default: %(default)s)',
-        default="**/params.py"
-    )
-    parser.add_argument(
-        "--offsets", "-o",
+        "--offsets",
         type=str,
         help="Optional name of CatGT text file with offsets for multiple concatenated recordings. (default: %(default)s)",
         default=None
     )
     parser.add_argument(
-        "--runit-path", "-r",
+        "--runit",
         type=str,
         help="Path to TPrime 'runit.sh' script. (default: %(default)s)",
         default="/opt/TPrime/TPrime-linux/runit.sh"
@@ -258,22 +341,30 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
     cli_args = parser.parse_args(argv)
 
+    catgt_path = Path(cli_args.catgt_root)
+    phy_path = Path(cli_args.phy_root)
+    output_path = Path(cli_args.output_root)
     try:
-        return run_tprime(
-            cli_args.catgt_root,
-            cli_args.phy_root,
-            cli_args.output_root,
-            cli_args.to_stream,
-            cli_args.from_streams,
-            cli_args.phy_from_pattern,
-            cli_args.phy_pattern,
+        find_runs_and_align(
+            catgt_path,
+            phy_path,
+            output_path,
+            cli_args.to_sync_pattern,
+            cli_args.events_sync_pattern,
+            cli_args.events_pattern,
+            cli_args.probe_ids,
+            cli_args.probe_sync_pattern,
+            cli_args.probe_spikes_pattern,
+            cli_args.probe_params_pattern,
             cli_args.sync_period,
             cli_args.offsets,
-            cli_args.runit_path
+            cli_args.runit,
         )
     except:
         logging.error("Error running tprime.", exc_info=True)
         return -1
+
+    return 0
 
 
 if __name__ == "__main__":
